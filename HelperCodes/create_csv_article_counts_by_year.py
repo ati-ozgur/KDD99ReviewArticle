621d494cf0216c2993b43f387687264116739a09
Atilla Ozgur <ati.ozgur@gmail.com>
2019-08-08 19:07:03 +0200

    python 3 changes

--- HelperCodes/create_csv_article_counts_by_year.py
+++ HelperCodes/create_csv_article_counts_by_year.py
@@ -11,8 +11,8 @@ df_article_count_by_year = df.year.value_counts().sort_index()
 csv_file_name = ReviewHelper.get_csv_save_folder() + "article_counts_by_year.csv"
 
 
-str1 = df_article_count_by_year.to_csv(path=None)
+str1 =  df_article_count_by_year.to_csv(header=False)
 
-with open(csv_file_name, 'wb') as f:
-    f.write("{0},{1}\n".format("Year" , "Count"))
+with open(csv_file_name, 'w') as f:
+    f.write("Year,Count\n")
     f.write(str1)
